80360c640189a67247051bc31db1d17d14346a87
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-05-17 17:58:17 +0900

    5/17　業務時間内途中経過

---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict'
+
+{
+  const todo = document.getElementById('todo');   
+  const submit = document.getElementById('submit');
+
+  // 登録ボタン
+submit.addEventListener('click', () => {
+  const todo = textboxElement.value;
+  textboxElement.value = '';
+
+  if(todo) {
+    todos.push(todo);
+    showTodos();
+  }
+
+});
+
+const showTodos {};
+
+item.done = false; // 完了はひとまずBoolean値で設定
+item.todo = todo.value 
+
+}
+
+}
