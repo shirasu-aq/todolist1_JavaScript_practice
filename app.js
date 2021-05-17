30f32fde224ce3e30b569a7ff21a62d587388fe5
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-05-18 00:29:27 +0900

    タスク登録（途中）

--- app.js
+++ app.js
@@ -1,41 +1,45 @@
 'use strict'
 
-
-  // 入力したTodoを配列として保存
-  const todos = [];
-
   // DOM操作
-const textbox = document.getElementById('input-todo-box');
-const addbutton =document.getElementById('submit');
-const todoListElement = document.getElementById('todo-list');
+  const addbutton =document.getElementById('submit');
+  const todoListElement = document.getElementById('todo-list');
+  const addtextbox = document.getElementById('input-todo-box');
 
-    // 登録ボタンを押した時の挙動
-  addbutton.addEventListener('click', () => {
-  // 変数todoにテキストボックスに入力した文字をvalueプロパティを用いて代入
-  const todo = textbox.value;
-  // 入力したテキストボックスを空にする
-  textbox.value = '';
+  // 登録したタスクを表示
+  const addTask = task => {
+    const listItem = document.createElement('li');
+    // <li>で表示されるテキストを、引数でとったタスクの文字列にする
+    listItem.innerText = task;
+    // 動的に子要素を追加
+    todoListElement.appendChild(listItem);
+  };
 
-  if(todo && todo.match(/\S/g)){
-    todos.push(todo);
-    showTodo(); 
-  }
+  // 登録ボタンに対してタスク登録イベントを設定
+  addbutton.addEventListener('submit', event => {
+    // インプット要素に入力された値を取得し、タスク登録メソッドに値を渡す
+    const task = addtextbox.value;
+    addTask(task);
+    addtextbox.value = '';
   });
 
-const showTodo = () => {
-    // HTML上の表示を一旦リセットする
-    while(todoListElement.firstChild) {
-      todoListElement.removeChild(todoListElement.firstChild);
-    }
-    
-    todos.forEach((todo, index) => {
-      // ulに追加するためにliタグの作成、todos内の配列を代入
-      const liElement = document.createElement("li");
-      // index値そのままを代入すると0からのスタートになるので、1を足す
-      liElement.textContent = `${index + 1} : ${todo}$`;
-});
 
-    //作成した要素をHTML上に表示させる
-    liElement.appendChild(deleteButton);
-    todoListElement.appendChild(liElement);
-};
+
+
+// ↓関係ないが残しておく
+
+  // // 入力したTodoを配列として保存
+  // const todos = [];
+
+
+  //   // 登録ボタンを押した時の挙動
+  // addbutton.addEventListener('click', () => {
+  // // 変数todoにテキストボックスに入力した文字をvalueプロパティを用いて代入
+  // // const todo = textbox.value;
+  // const todo = document.createElement('li');
+  // // 入力したテキストボックスを空にする
+  // textbox.value = '';
+
+  // todo.innerText = textbox.value;
+  // todoListElement.appendChild(todo);
+  // });
+  
