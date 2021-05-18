9bd2eae13080a5fc1fca3cb54ff9f2dfb311df15
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-05-18 20:05:12 +0900

    キャメルケースに修正

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 "use strict";
 
 // DOM操作
-const addbutton = document.getElementById("submit");
+const addButton = document.getElementById("submit");
 const todoListElement = document.getElementById("todo-list");
-const addtextbox = document.getElementById("input-todo-box");
+const addTextbox = document.getElementById("input-todo-box");
 
 // 登録したタスクを表示
 const addTask = (task) => {
   // チェックボタン表示
-  const checkbutton = document.createElement("input");
-  checkbutton.setAttribute("type", "checkbox"); // [or] input.type = 'checkbox';
+  const checkButton = document.createElement("input");
+  checkButton.setAttribute("type", "checkbox"); // [or] input.type = 'checkbox';
   // checkbutton.setAttribute("checked", "checked"); // [or] input.checked = true;
 
   // ×ボタン表示
-  const deletebutton = document.getElementById("delete");
+  const deleteButton = document.getElementById("delete");
 
   const listItem = document.createElement("li");
   // <li>で表示されるテキストを、引数でとったタスクの文字列にする
@@ -21,17 +21,17 @@ const addTask = (task) => {
 
   // 動的に子要素を追加
   todoListElement.appendChild(listItem);
-  listItem.append(deletebutton, checkbutton);
+  listItem.append(deleteButton, checkButton);
 };
-console.log(addbutton);
+
 // 登録ボタンに対してタスク登録イベントを設定
-addbutton.addEventListener("submit", (event) => {
+addButton.addEventListener("submit", (event) => {
   console.log("クリック");
   // デフォルトの処理をキャンセルする
   event.preventDefault();
 
   // インプット要素に入力された値を取得し、タスク登録メソッドに値を渡す
-  const task = addtextbox.value;
+  const task = addTextbox.value;
   addTask(task);
-  addtextbox.value = "";
+  addTextbox.value = "";
 });
