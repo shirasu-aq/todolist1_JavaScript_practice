2e4986e19ee21480160b1d19e181c60f96e1f70b
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-05-18 23:54:57 +0900

    Todoリスト表示完成

--- app.js
+++ app.js
@@ -7,21 +7,29 @@ const addTextbox = document.getElementById("input-todo-box");
 
 // 登録したタスクを表示
 const addTask = (task) => {
+  // liタグの生成
+  const listItem = document.createElement("li");
+
+  // ×ボタン表示
+  const deleteButton = document.createElement("span");
+  deleteButton.innerText = "×";
+  deleteButton.setAttribute("id", "delete");
+  listItem.append(deleteButton);
+
   // チェックボタン表示
   const checkButton = document.createElement("input");
   checkButton.setAttribute("type", "checkbox"); // [or] input.type = 'checkbox';
   // checkbutton.setAttribute("checked", "checked"); // [or] input.checked = true;
+  listItem.append(checkButton);
 
-  // ×ボタン表示
-  const deleteButton = document.getElementById("delete");
-
-  const listItem = document.createElement("li");
-  // <li>で表示されるテキストを、引数でとったタスクの文字列にする
-  listItem.innerText = task;
+  // 登録されたテキストを要素で表示（ラベル要素使用）
+  const todoDescription = document.createElement("label");
+  // 入力されたテキストを引数でとった文字列にする
+  todoDescription.innerText = task;
+  listItem.append(todoDescription);
 
   // 動的に子要素を追加
   todoListElement.appendChild(listItem);
-  listItem.append(deleteButton, checkButton);
 };
 
 // 登録ボタンに対してタスク登録イベントを設定
