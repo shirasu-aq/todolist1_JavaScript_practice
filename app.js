9ce5fc97430a7181d96da2d326dd2a7ec1d82fa3
tatsuya-asami <tatsuya.asami@asia-quest.jp>
2021-05-21 18:07:30 +0900

    ADDを押したらliタグを追加する

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 "use strict";
 
-console.log("loaded");
+/**
+ * ADDを押す(id='submit'のsubmitメソッドを実行す)
+ *
+ * ADDとイベントを結びつける
+ * 追加する内容を作る
+ *
+ * 行が追加される
+ */
+
+// ADDを押したら(id='submit'のsubmitメソッドを実行したら)行を追加する
+const formTag = document.getElementById("submit");
+formTag.addEventListener("submit", (event) => {
+  const listTag = document.createElement("li");
+  const ulTag = document.getElementById("todo-list");
+  ulTag.appendChild(listTag);
+
+  event.preventDefault();
+});
