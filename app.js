5625408200da743bd1377d5d5ce14be029e0d6d5
tatsuya-asami <tatsuya.asami@asia-quest.jp>
2021-05-21 18:09:22 +0900

    liタグの名前を修正

--- app.js
+++ app.js
@@ -7,14 +7,17 @@
  * 追加する内容を作る
  *
  * 行が追加される
+ * バツボタンを追加する
+ * チェックボタンを追加する
+ * inputした内容を追加する
  */
 
 // ADDを押したら(id='submit'のsubmitメソッドを実行したら)行を追加する
 const formTag = document.getElementById("submit");
 formTag.addEventListener("submit", (event) => {
-  const listTag = document.createElement("li");
+  const liTag = document.createElement("li");
   const ulTag = document.getElementById("todo-list");
-  ulTag.appendChild(listTag);
+  ulTag.appendChild(liTag);
 
   event.preventDefault();
 });
