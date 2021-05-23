249409bde1704cc520f2284bd1f5efca2a8f5fee
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-05-23 15:38:15 +0900

    クリックでリスト表示

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const addButton = document.getElementById("submit");
+
+// アドボタンを押してul表示;
+addButton.addEventListener("click", () => {
+  const taskHolder = document.getElementById("todo-list");
+  const taskList = document.createElement("li");
+  taskHolder.appendChild(taskList);
+  event.preventDefault();
+});
+
+// const todoList = document.getElementById("to-dolist");
